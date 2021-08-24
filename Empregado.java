ef75f58c0599662752660f295d6b6cd16a8d5e68
Gustavo Lopes <gustalopes_sdm@hotmail.com>
2021-08-24 20:44:58 -0300

    Aula 1 - Primeira modificação no git

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,28 @@
 public class Empregado{
     private String nome;
     private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+
+    public double calculaSalario(){
+        if(tipo == 1){
+            return salario;
+        }
+
+        else if(tipo == 2){
+            return salario + salario * comissao;
+        }
+
+        else if(tipo == 3){
+            return salario + bonus;
+        }
+        
+        else{
+            return 0;
+        }
+    }
 
     public String getNome(){
         return this.nome;
