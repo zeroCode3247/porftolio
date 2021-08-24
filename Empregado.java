631f76567a41cdb10bc8135d22b9333f21a744ed
Gustavo Lopes <gustalopes_sdm@hotmail.com>
2021-08-24 20:32:30 -0300

    Aula 1 - Primeira classe no git

---
+++ Empregado.java
@@ -0,0 +1,20 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public void setNome(int idade){
+        this.idade = idade;
+    }
+}
