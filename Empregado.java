d96bd6db075dfa1ad1a4fcf0e72386775c0b0cb5
Gustavo Lopes <gustalopes_sdm@hotmail.com>
2021-08-26 00:05:12 -0300

    Teste concluído com sucesso e .gitignore adicionado

--- Empregado.java
+++ Empregado.java
@@ -24,6 +24,30 @@ public class Empregado{
         }
     }
 
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    public double getSalario(){
+        return this.salario;
+    }
+
+    public void setComissao(double comissao){
+        this.comissao = comissao;
+    }
+
+    public double getComissao(){
+        return this.comissao;
+    }
+
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
+    
+    public double getBonus(){
+        return this.bonus;
+    }
+
     public String getNome(){
         return this.nome;
     }
